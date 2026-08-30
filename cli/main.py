764eff74d84046e715fe99d9ffdd8d8bf7c13d46
DeeeeeeM <65114929+DeeeeeeM@users.noreply.github.com>
2026-08-31 00:31:46 +0800

    feat: added cli support for organizer function

--- cli/main.py
+++ cli/main.py
@@ -1,7 +1,39 @@
+import argparse
 from pathlib import Path
+from modules.organizer import organize_files
+from rich.panel import Panel
+from rich.console import Console
 
-input_directory = Path("C:/Users/Heidel/Downloads")
+parser = argparse.ArgumentParser(
+    prog="forgesort",
+    usage="forgesort [command] [options] <folder>",
+    description="ForgeSort is a file management utility for organizing files, bulk renaming, and duplicate detection.",
+    epilog="Examples: \nforgesort organize Downloads\nforgesort rename Photos\nforgesort duplicates Documents",
+    formatter_class=argparse.RawDescriptionHelpFormatter
+)
+parser.add_argument("-o", "--organize", required=True, help="organizes files inside a specified folder", type=Path)
+args = parser.parse_args()
 
-#SCAN FILES
-folder_files = [i for i in input_directory.iterdir() if i.is_file()]
-file_extension = input_directory.suffix
+folder_files = [i for i in args.organize.iterdir() if i.is_file()]
+
+console = Console()
+
+import time
+
+with console.status("Scanning files..."):
+    time.sleep(5)
+    organize_files(folder_files, args.organize)
+
+config = (
+    f"Input: {args.organize}\n"
+    f"Mode: Organizer\n"
+    f"[green]Status: {len(folder_files)} files processed[/green]"
+)
+    
+console.print(
+    Panel.fit(
+        config,
+        title="Scan Configuration",
+        border_style="cyan"
+    )
+)
--- modules/organizer.py
+++ modules/organizer.py
@@ -1,25 +1,33 @@
 from pathlib import Path
 from shutil import move
-from organizer.file_extensions import FILE_TYPE_EXTENSIONS
+from modules.file_extensions import FILE_TYPE_EXTENSIONS
+from rich.progress import Progress
         
 
-def organize_files(folder_files, input_directory : Path):
-    for file in folder_files:
-        file_ext = file.suffix.lower()
-        matched = False
-        
-        for category, values in FILE_TYPE_EXTENSIONS.items():
-            if file_ext in values:
-                output_dir = input_directory / category
-                output_dir.mkdir(parents=True, exist_ok=True)                
-                
-                if (output_dir / file.name).exists(): continue
-                
-                move(file, output_dir)
-                matched = True
-                break   
+def organize_files(folder_files : list, input_directory : Path):
+    
+    total_files = len(folder_files)
+    
+    with Progress() as progress:
+        task = progress.add_task("Organizing files...", total=total_files)
+    
+        for item in folder_files:
+            file_ext = item.suffix.lower()
+            matched = False
             
-        if matched == False:
-            output_dir = input_directory / "Other Files"
-            output_dir.mkdir(parents=True, exist_ok=True)
-            move(file, output_dir)
+            for category, values in FILE_TYPE_EXTENSIONS.items():
+                if file_ext in values:
+                    output_dir = input_directory / category
+                    output_dir.mkdir(parents=True, exist_ok=True)            
+                    if (output_dir / item.name).exists(): 
+                        continue
+                    move(item, output_dir)
+                    matched = True
+                    progress.update(task, advance=1)
+                    break   
+            
+            if matched == False:
+                output_dir = input_directory / "Other Files"
+                output_dir.mkdir(parents=True, exist_ok=True)
+                move(item, output_dir)
+                progress.update(task, advance=1)
