1b45859bd1e6266daa43bad3e7ae5c70b0c5aef3
DeeeeeeM <65114929+DeeeeeeM@users.noreply.github.com>
2026-09-03 22:05:42 +0800

    feat: added check duplicate cli args

--- cli/main.py
+++ cli/main.py
@@ -1,10 +1,10 @@
 import argparse
 from pathlib import Path
-from modules.helper import organize_files
-from modules.helper import scan_files
 from rich.panel import Panel
 from rich.console import Console
-
+from modules.helper import organize_files
+from modules.helper import scan_files
+from modules.helper import check_duplicate
 
 # CLI ARG PARSER
 parser = argparse.ArgumentParser(
@@ -14,27 +14,65 @@ parser = argparse.ArgumentParser(
     epilog="Examples: \nforgesort organize Downloads\nforgesort rename Photos\nforgesort duplicates Documents",
     formatter_class=argparse.RawDescriptionHelpFormatter
 )
-parser.add_argument("-o", "--organize", required=True, help="organizes files inside a specified folder", type=Path)
-args = parser.parse_args()
 
-folder_files = scan_files(args.organize)
+parser.add_argument("-o", "--organize", help="organizes files inside a specified folder", type=Path)
+parser.add_argument("-ch", "--checkdup", help="Check any duplicate files inside a specified folder", type=Path)
+args = parser.parse_args()
 
 # RICH CONSOLE
 console = Console()
 
-with console.status("Organizing files..."):
-    organize_files(folder_files, args.organize)
-
-config = (
-    f"Input: {args.organize}\n"
-    f"Mode: Organizer\n"
-    f"[green]Status: {len(folder_files)} files processed[/green]"
-)
-    
-console.print(
-    Panel.fit(
-        config,
-        title="Scan Configuration",
-        border_style="cyan"
+if args.organize:
+    folder_files = scan_files(args.organize)
+    with console.status("Organizing..."):
+        organize_files(folder_files, args.organize)
+    config = (
+        f"Input: {args.organize}\n"
+        f"Mode: File Organizer\n"
+        f"[green]Status: {len(folder_files)} files processed[/green]"
     )
-)
+    console.print(
+        Panel.fit(
+            config,
+            title="Scan Configuration",
+            border_style="cyan"
+        )
+    )
+
+elif args.checkdup:
+    folder_files = scan_files(args.checkdup)
+    with console.status("Checking..."):
+        duplicate_files = check_duplicate(folder_files)
+        num_duplicate = sum(len(group) for group in duplicate_files)
+    config = (
+        f"Input: {args.checkdup}\n"
+        f"Mode: Check Duplicate Files\n"
+        f"[green]Status: {len(folder_files)} files processed[/green]\n"
+        f"[yellow]Number of duplicate files: {num_duplicate}[/yellow]"
+    )
+    console.print(
+        Panel.fit(
+            config,
+            title="Checking Report",
+            border_style="cyan"
+        )
+    )
+    
+    config_files = "\n".join(
+        file.name  
+        for group in duplicate_files
+        for file in group
+    )
+    
+    console.print(
+        Panel.fit(
+            config_files,
+            title="File Duplicates",
+            border_style="cyan"
+        )
+    )
+    
+    
+        
+    
+    
